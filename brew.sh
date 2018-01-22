99bd527df647e41c57053a966a43370f2594be9d
Eugene Bravov <eugene.bravov@gmail.com>
2018-01-22 21:23:03 +0300

    Remove unused brews

--- brew.sh
+++ brew.sh
@@ -45,43 +45,36 @@ brew install screen
 brew install homebrew/php/php71
 brew install homebrew/php/php-cs-fixer
 brew install python3
-pip3 install neovim
 brew cask install ngrok
 brew cask install android-file-transfer
 brew cask install insomnia
 brew cask install kap
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+# brew install aircrack-ng
+# brew install bfg
+# brew install binutils
+# brew install binwalk
+# brew install cifer
+# brew install dex2jar
+# brew install dns2tcp
+# brew install fcrackzip
+# brew install foremost
+# brew install hashpump
+# brew install hydra
+# brew install john
+# brew install knock
+# brew install netpbm
+# brew install nmap
+# brew install pngcheck
+# brew install socat
+# brew install sqlmap
+# brew install tcpflow
+# brew install tcpreplay
+# brew install tcptrace
+# brew install ucspi-tcp # `tcpserver` etc.
+# brew install xpdf
+# brew install xz
 
 # Install other useful binaries.
 brew install ack
@@ -89,7 +82,7 @@ brew install ack
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
-brew install lua
+# brew install lua
 brew install lynx
 brew install p7zip
 brew install pigz
@@ -139,3 +132,4 @@ brew cask install webpquicklook
 
 # Remove outdated versions from the cellar.
 brew cleanup
+brew cask cleanup
